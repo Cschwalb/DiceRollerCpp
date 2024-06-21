de8e0f504c4b713ae6225e982e9fb54e8a815fd1
Caleb Schwalb <calebschwalb@gmail.com>
2024-06-20 22:38:40 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <tuple>
+#include <random>
+
+
+
+class Dice
+{
+private:
+    int m_nMax;
+    int m_nNumberOfDice;
+public:
+    int getMax()
+    {
+        return this->m_nMax;
+    }
+
+    int getNumberOfDice()
+    {
+        return this->m_nNumberOfDice;
+    }
+
+    Dice(int max, int number)
+    {
+        this->m_nMax = max;
+        this->m_nNumberOfDice = number;
+    }
+
+    int rollDice()
+    {
+        int total = 0;
+        for(int index = 0; index < this->getNumberOfDice(); index++)
+        {
+            std::random_device rd;
+
+            std::mt19937 mt(rd());
+            std::uniform_int_distribution<int> dist(1, this->m_nMax);
+            int randomNumber = dist(mt);
+            total += randomNumber;
+        }
+        return total;
+    }
+};
+
+std::vector<std::string> split(std::string& splitme, char delimiter)
+{
+    std::vector<std::string> results;
+    std::stringstream ss(splitme);
+    std::string token;
+    while(getline(ss, token, delimiter))
+    {
+        results.push_back(token);
+    }
+    return results;
+}
+
+std::tuple<int, int> parseContent(std::string Input) // returns first and last number
+{
+    std::vector<std::string> numbers = split(Input, 'd');
+    int first, last;
+    first = std::stoi(numbers.at(0));
+    last = std::stoi(numbers.at(1));
+
+    std::tuple<int, int> ret = std::make_tuple(first, last);
+    return ret;
+}
+int main() {
+    std::tuple<int, int> ret = parseContent("1d20");
+    Dice rolling = Dice(std::get<1>(ret), std::get<0>(ret));
+    std::cout<<rolling.rollDice();
+    return 0;
+}
